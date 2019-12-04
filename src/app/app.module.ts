609eb559aa0407fd1c470bbf972aef3e2a866e24
McTNTCrafter // luca <luca.guettinger@gmail.com>
2019-12-04 21:51:19 +0100

    delete project and generate new one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BuslistComponent } from './buslist/buslist.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatTableModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BuslistComponent
+    AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatButtonModule,
-    MatInputModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

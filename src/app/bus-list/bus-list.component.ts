fffd016b12208c622e8b8ce894b26bd9fb92bee7
McTNTCrafter // luca <luca.guettinger@gmail.com>
2019-12-05 19:06:25 +0100

    more or less implement buslist and bustype list

--- src/app/bus-list/bus-list.component.ts
+++ src/app/bus-list/bus-list.component.ts
@@ -8,12 +8,13 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'bm-bus-list',
   templateUrl: './bus-list.component.html',
-  styleUrls: ['./bus-list.component.css'],
+  styleUrls: ['./bus-list.component.css', './../styles/expandable-table-style.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('100ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+      state('collapsed, void', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ])
   ]
 })
--- src/app/bustype-list/bustype-list.component.ts
+++ src/app/bustype-list/bustype-list.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {BusType} from '../model/bus';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Bus, BusType} from '../model/bus';
 import {BusService} from '../bus.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'bm-bustype-list',
   templateUrl: './bustype-list.component.html',
-  styleUrls: ['./bustype-list.component.css', 'src/app/styles/expandable-table-style.css']
+  styleUrls: ['./bustype-list.component.css', './../styles/expandable-table-style.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed, void', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
+  ]
 })
 export class BustypeListComponent implements OnInit {
-  private data: BusType[];
   displayedColumns: string[] = ['name', 'capacity', 'recoveryTime', 'maxRange', 'distancePerH'];
-  private baseUrl = '/base/bus/';
-
+  private dataSource: MatTableDataSource<BusType>;
+  expandedElement: BusType | null;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private busService: BusService) { }
 
   ngOnInit() {
     this.busService.getBusTypes().subscribe(value => {
-      this.data = value;
+      this.dataSource = new MatTableDataSource(value);
+      this.dataSource.sort = this.sort;
     });
   }
 
